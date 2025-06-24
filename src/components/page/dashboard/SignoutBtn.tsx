1404fef04d065b6f2b5d7348081d340d8a3d08d6
MohamedAbuzaidDev <mo.adel.developer@gmail.com>
2025-06-25 00:03:05 +0300

    try solve logout 3 version 1.0.0

--- src/components/page/dashboard/SignoutBtn.tsx
+++ src/components/page/dashboard/SignoutBtn.tsx
@@ -1,19 +1,14 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useRevalidate } from "@/hooks/revalidate";
-import { userSignOut } from "@/lib/Signout";
-import { signOut as nextAuthSignout } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 export default function SignoutBtn() {
   const { revalidate } = useRevalidate();
-  const handleSignout = async () => {
-    await userSignOut()
-    await nextAuthSignout()
-  }
   return (
     <Button onClick={async () => {
         await revalidate("/", "*");
-        handleSignout()
+        signOut()
     }} className="bg-white hover:bg-red-500 text-gray-800 hover:text-white font-bold border border-gray-300 flex items-center justify-center gap-3 relative overflow-hidden group">
       تسجيل خروج
     </Button>
--- src/lib/Signout.ts
+++
@@ -1,7 +0,0 @@
-"use server"
-
-import { signOut } from "next-auth/react"
-
-export const userSignOut = async () => {
-    await signOut({redirect: false})
-}
