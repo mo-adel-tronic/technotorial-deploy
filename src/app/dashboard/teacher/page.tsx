c5b5d16d552d5c8d3461228f553aeb70a5eec97d
MohamedAbuzaidDev <mo.adel.developer@gmail.com>
2025-06-25 00:49:58 +0300

    try solve data retreive 1 version 1.0.0

--- src/app/dashboard/teacher/page.tsx
+++ src/app/dashboard/teacher/page.tsx
@@ -5,6 +5,7 @@ import { fetchAllTeachers } from "@/features/teachers/TeacherRepo";
 
 export default async function Teacher() {
   const teachers = await fetchAllTeachers()
+  console.log(teachers)
   return (
     <>
     <HeaderBanner title='إدارة أعضاء هيئة التدريس' linkCreate={{
