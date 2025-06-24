2505d931c5c06591913657a911202b43bb1adc53
MohamedAbuzaidDev <mo.adel.developer@gmail.com>
2025-06-25 00:29:47 +0300

    try solve logout 7 version 1.0.0

--- src/components/page/dashboard/SignoutBtn.tsx
+++ src/components/page/dashboard/SignoutBtn.tsx
@@ -3,17 +3,27 @@ import { Button } from "@/components/ui/button";
 import { useRevalidate } from "@/hooks/revalidate";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function SignoutBtn() {
   const { revalidate } = useRevalidate();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   return (
-    <Button onClick={async () => {
+    <Button
+      onClick={async () => {
+        setIsLoading(true);
         await revalidate("/", "*");
-        signOut({redirect: false})
+        await signOut({ redirect: false });
         router.push("/login");
-    }} className="bg-white hover:bg-red-500 text-gray-800 hover:text-white font-bold border border-gray-300 flex items-center justify-center gap-3 relative overflow-hidden group">
-      تسجيل خروج
+        setIsLoading(false);
+      }}
+      disabled={isLoading}
+      className="bg-white hover:bg-red-500 text-gray-800 hover:text-white font-bold border border-gray-300 flex items-center justify-center gap-3 relative overflow-hidden group"
+    >
+      <span className="relative font-bold">
+        {isLoading ? "جاري تسجيل الخروج..." : "تسجيل خروج"}
+      </span>
     </Button>
   );
 }
